f0d35e4d244d8cdc81f23248733d528ff4ffe112
Isaac V <vanmeciv@gmail.com>
2020-04-19 13:13:02 -0700

    changing initial map extent and setview

--- OPORD1/scripts.js
+++ OPORD1/scripts.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([46.933, -123.1308], 11);
+var mymap = L.map('mapid').setView([46.881, -123.2014], 10.45);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
