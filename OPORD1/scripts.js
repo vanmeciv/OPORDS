06d46ad3e5ea025a288abf99539eea674f2b85d6
Isaac V <vanmeciv@gmail.com>
2020-04-19 09:26:24 -0700

    adding notes and marker

--- OPORD1/scripts.js
+++ OPORD1/scripts.js
@@ -3,23 +3,23 @@ var mymap = L.map('mapid').setView([46.732, -122.8], 8);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/outdoors-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiaXNhYWN2IiwiYSI6ImNrMnpqYnVxaTA1b3IzbXBnaG5zY3o3eTEifQ.kMdIcXYBFKHTorj3Hxgi7g'
 }).addTo(mymap);
 
 
+
+var marker = L.marker([	46.8721964, -123.1930277]).addTo(map);
 //
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-//
-// var circle = L.circle([51.508, -0.11], {
+// var circle = L.circle([	46.8721964, -123.1930277], {
 // 	color: 'red',
 // 	fillColor: '#f03',
 // 	fillOpacity: 0.5,
 // 	radius: 750
-// }).addTo(map);
-//
+// }).addTo(mymap);
+
 // var polygon = L.polygon([
 // 	[51.509, -0.08],
 // 	[51.503, -0.06],
