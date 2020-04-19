9aba81852757c51d38fd669de3b1592482152b44
Isaac V <vanmeciv@gmail.com>
2020-04-19 09:47:26 -0700

    markers are plotted & 1 icon example and shadow needs hosting

--- OPORD1/scripts.js
+++ OPORD1/scripts.js
@@ -10,14 +10,48 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 
+// OBJ Dragoon
+// var dragoon = L.marker([	46.8721964, -123.1930277]).addTo(mymap);
+// OBJ Chestnut
+var chestnut = L.marker([	46.8793401, -123.2235009]).addTo(mymap);
+// OBJ Blackjack
+var blackjack = L.marker([	46.8579105, -123.1792019]).addTo(mymap);
+// FOB Spartan
+var spartan = L.marker([	46.8472339, -123.2177278]).addTo(mymap);
+// Arianan HQ
+var ariananHQ = L.marker([	46.8795426, -123.0599767]).addTo(mymap);
 
-var marker = L.marker([	46.8721964, -123.1930277]).addTo(map);
-//
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: 'leaf-shadow.png',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
+
+var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'}),
+    redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
+    orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
+
+
+    L.icon = function (options) {
+        return new L.Icon(options);
+    };
+
+L.marker([46.8721964, -123.1930277], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+L.marker([51.495, -0.083], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
+L.marker([51.49, -0.1], {icon: orangeIcon}).addTo(map).bindPopup("I am an orange leaf.");
+
+
 // var circle = L.circle([	46.8721964, -123.1930277], {
 // 	color: 'red',
 // 	fillColor: '#f03',
 // 	fillOpacity: 0.5,
-// 	radius: 750
+// 	radius: 1200
 // }).addTo(mymap);
 
 // var polygon = L.polygon([
@@ -29,3 +63,16 @@ var marker = L.marker([	46.8721964, -123.1930277]).addTo(map);
 // marker.bindPopup("<b>Hello world!</b><br>I am a popup.")
 // circle.bindPopup("I am a circle.");
 // polygon.bindPopup("I am a Haunted polygon.");
+
+// L.marker([46.8721964, -123.1930277], {icon: greenIcon}).addTo(mymap);
+//
+// var greenIcon = L.icon({
+//     iconUrl: 'leaf-green.png',
+//     shadowUrl: 'leaf-shadow.png',
+//
+//     iconSize:     [38, 95], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
+//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     shadowAnchor: [4, 62],  // the same for the shadow
+//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
